05c527f4c2a921f266ddc525fbfb6eb470618c9a
오성이 <hospi0539@naver.com>
2025-05-27 02:23:44 +0900

    “Comment”

---
+++ bye.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "bye.h"
+#include "myinfo.h"
+
+void printBye(void){
+
+    printf("Bye %s\n", MY_NAME);
+
+}
---
+++ bye.h
@@ -0,0 +1,6 @@
+#ifndef BYE_H
+#define BYE_H
+
+void printBye(void);
+
+#endif
